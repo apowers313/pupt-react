ec6b7ce197fe0c66751774a03835b6dcb7d67096
Adam Powers <apowers@ato.ms>
2026-02-03 21:40:22 -0800

    ci: add coverage and coveralls.io

--- vitest.config.ts
+++ vitest.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     },
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json", "html", "lcov"],
       include: ["src/**/*.{ts,tsx}"],
       exclude: [
         "src/**/*.d.ts",
